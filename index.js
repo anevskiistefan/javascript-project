a2affac8fa7b5f839ae6aa63be2a48d91630ea20
anevskiistefan <52758745+anevskiistefan@users.noreply.github.com>
2019-09-08 08:16:57 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var registerButton = document.querySelector(".register");
-var usersButton = document.querySelector(".users");
 var loginButton = document.querySelector(".login");
 var input1 = document.getElementsByTagName("input")[0];
 var input2 = document.getElementsByTagName("input")[1];
 var ol = document.querySelector("ol");
+var a = document.querySelectorAll("a");
 var users = [];
 var userUsername;
 var userPassword;
 
-registerButton.addEventListener("click", function() {
+
+// FUNCTIONS
+
+registerButton.addEventListener("click", function () {
   userUsername = prompt("What will be your username?");
   userPassword = prompt("What will be your password?");
 
@@ -16,34 +19,38 @@ registerButton.addEventListener("click", function() {
     username: userUsername,
     password: userPassword
   });
-
-  addToUl()
-
+  addLink();
 });
 
-function addToUl() {
-  var li = document.createElement("li");                 
-  li.appendChild(document.createTextNode(userUsername));         
-  ol.appendChild(li);     
+function addLink() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(userUsername));
+  ol.appendChild(li);
 }
 
-loginButton.addEventListener("click", function() {
+
+loginButton.addEventListener("click", function () {
+  login();
+});
+
+
+
+
+function login() {
   if (input1.value.length === 0 || input2.value.length === 0) {
     return alert("Please write your username and password");
   } else {
-    for(var i=0; i < users.length; i++) {
-      if(input1.value === users[i].username && input2.value === users[i].password) {
+    for (var i = 0; i < users.length; i++) {
+      if (input1.value.toLowerCase() === users[i].username && input2.value.toLowerCase() === users[i].password) {
         input1.value = "";
         input2.value = "";
         return alert("Successfully loged in!");
       }
     }
-        input1.value = "";
-        input2.value = "";
+    input1.value = "";
+    input2.value = "";
     return alert("Try again!");
-    }
-});
+  }
+}
 
-usersButton.addEventListener("click", function() {
-  console.log(users);
-});
+
